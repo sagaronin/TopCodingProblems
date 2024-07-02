f24f4b6501ca61b8872ccdd199cd1f7b58b7352b
Sagar Hagwane <sagar.hagwane13@gmail.com>
2024-07-01 23:05:20 -0700

    find min bouquet binary search

--- src/binary_search/MinBouquet.java
+++ src/binary_search/MinBouquet.java
@@ -4,35 +4,31 @@ public class MinBouquet {
     public static void main(String[] args) {
         // Return the minimum integer K such that
         // Koko can eat all bananas in H hours
-        int bloomingDays[] = { 7,7,7,7,13,11,12,7 };
-        System.out.println("Min Bouquet : " + findMinBouquetBrute(bloomingDays, 2,3));
+        int bloomingDays[] = { 7, 7, 7, 7, 13, 11, 12, 7 };
+        System.out.println("Min Bouquet(Brute Force) : " + findMinBouquetBrute(bloomingDays, 2, 3));
+        System.out.println("Min Bouquet(Binary Search) : " + findMinBouquetBS(bloomingDays, 2, 3));
     }
-    public static int findMinBouquetBrute(int arr[], int noOfBouquets, int adjacent){
-        for (int i = getMinValue(arr); i <= getMaxValue(arr); i++) {
-            if(possibleCombination(arr, i, noOfBouquets, adjacent)==true)
+
+    public static int findMinBouquetBrute(int arr[], int noOfBouquets, int adjacent) {
+        int maxMin[] = getMaxMinValue(arr);
+        for (int i = maxMin[1]; i <= maxMin[0]; i++) {
+            if (possibleCombination(arr, i, noOfBouquets, adjacent) == true)
                 return i;
         }
         return -1;
     }
-    public static int getMaxValue(int[] array) {
+
+    public static int[] getMaxMinValue(int[] array) {
         int maxValue = array[0];
+        int minValue = array[0];
         for (int i = 1; i < array.length; i++) {
             if (array[i] > maxValue) {
                 maxValue = array[i];
-            }
-        }
-        return maxValue;
-    }
-    
-    // getting the miniumum value
-    public static int getMinValue(int[] array) {
-        int minValue = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < minValue) {
+            } else if (array[i] < minValue) {
                 minValue = array[i];
             }
         }
-        return minValue;
+        return new int[] { maxValue, minValue };
     }
 
     public static boolean possibleCombination(int[] arr, int day, int noOfBouquet, int adjacentBoquets) {
@@ -51,5 +47,23 @@ public class MinBouquet {
             return true;
         else
             return false;
+    }
+
+    public static int findMinBouquetBS(int arr[], int noOfBouquets, int adjacent) {
+        if (noOfBouquets * adjacent > arr.length)
+            return -1;
+        int maxMin[] = getMaxMinValue(arr);
+        int low = maxMin[1], high = maxMin[0];
+        int ans = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (possibleCombination(arr, mid, noOfBouquets, adjacent) == true) {
+                ans = mid;
+                high = mid - 1;
+            } else
+                low = mid + 1;
+
+        }
+        return ans;
     }
 }

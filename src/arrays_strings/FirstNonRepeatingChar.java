3cc638ffe8bd106d448d2de314a88174e4081510
Sagar Hagwane <sagar.hagwane13@gmail.com>
2025-03-19 15:31:14 -0400

    Using shortcut method in maps to store or update value

--- src/arrays_strings/FirstNonRepeatingChar.java
+++ src/arrays_strings/FirstNonRepeatingChar.java
@@ -35,11 +35,14 @@ public class FirstNonRepeatingChar {
         Map<Character, Integer> map = new HashMap<Character, Integer>();
         
         for (int i = 0; i < str.length(); i++) {
-            if(map.containsKey(str.charAt(i))){
-                map.put(str.charAt(i), map.get(str.charAt(i))+1);
-            }else{
-                map.put(str.charAt(i), 1);
-            }
+            // if(map.containsKey(str.charAt(i))){
+            //     map.put(str.charAt(i), map.get(str.charAt(i))+1);
+            // }else{
+            //     map.put(str.charAt(i), 1);
+            // }
+
+            map.put(str.charAt(i),map.getOrDefault(str.charAt(i), 0)+1);
+            
         }
         for (int i = 0; i < str.length(); i++) {
             if(map.get(str.charAt(i))==1){
